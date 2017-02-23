251662502b47ed2122c5c00b3337787e17fab52e
Shubham Mahajan <shubhammahajan@gmail.com>
2017-02-22 18:46:15 -0800

    Connected the app to the Firebase database

--- app/src/main/java/com/example/shubham/firebasetutorial/MainActivity.java
+++ app/src/main/java/com/example/shubham/firebasetutorial/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.shubham.firebasetutorial;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainActivity extends AppCompatActivity {
-    String text = "";
+    private Button sendData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sendData = (Button) findViewById(R.id.send_message);
+
+
+    }
+
+    public void OnClick(View view) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef= database.getReference();
+
+        myRef.setValue("Hello World");
     }
 }
